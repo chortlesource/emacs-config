67059e9dde0737e4c906857f90fdf3c7f9fb802f
chortlesource <chris.m.short@outlook.com>
2021-04-01 19:05:37 +0100

    Added C/C++ dev plugins

---
+++ .emacs.d/config/cc-mode-projects.el
@@ -0,0 +1,4 @@
+(ede-cpp-root-project "ink"
+                      :file "/home/chris/Documents/c++/ncurses/ink/CMakeLists.txt"
+                      :include-path '("inc" "inc/comp" "inc/sys")
+                      :system-include-path '("/usr/include"))
