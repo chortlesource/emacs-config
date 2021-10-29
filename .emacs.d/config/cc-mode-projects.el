4500512ad7a47af6078e86f542d64e76fde25b41
Chris .M. Short <chris.m.short@outlook.com>
2021-10-29 20:14:49 +0100

    Removed unused C++ Dev

--- .emacs.d/config/cc-mode-projects.el
+++
@@ -1,4 +0,0 @@
-(ede-cpp-root-project "ink"
-                      :file "/home/chris/Documents/c++/ncurses/ink/CMakeLists.txt"
-                      :include-path '("inc" "inc/comp" "inc/sys")
-                      :system-include-path '("/usr/include"))
